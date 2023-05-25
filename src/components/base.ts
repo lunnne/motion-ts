e9846fe6914a4fbd5094f4ef72519759309c09e0
Jin <munjin1340@gmail.com>
2023-05-25 23:05:13 +0200

    onClose completed

--- src/components/base.ts
+++ src/components/base.ts
@@ -1,5 +1,6 @@
 export interface Component {
     attachTo(parent: HTMLElement, position?: InsertPosition) : void;
+    removeFrom(parent:HTMLElement) : void;
 }
 // Encapsulate the HTML element creation 
 // HTML Element 를 만드는 과정을 캡슐화 해준다.
@@ -14,5 +15,12 @@ export class BaseComponent<T extends HTMLElement> implements Component{
   }
   attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     parent.insertAdjacentElement(position, this.element);
+  }
+
+  removeFrom(parent: HTMLElement): void {
+    if(parent !== this.element.parentElement) {
+      throw new Error ('Parent mismatch!')
+    }
+    parent.removeChild(this.element)
   }
 }
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,6 +4,8 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void;
+
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
   constructor() {
     super('<ul class="page"></ul>');
@@ -12,10 +14,14 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(()=> {
+     item.removeFrom(this.element) 
+    })
   }
 }
 
 export class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  private closeListener: OnCloseListener | undefined;
   constructor() {
     super(`<li class="page-item">
         <section class="page-item_body"></section>
@@ -23,10 +29,18 @@ export class PageItemComponent extends BaseComponent<HTMLElement> implements Com
           <button class="close">&times;</button>
         </div>
       </li>`);
+    const deleteBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    deleteBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
 
   addChild(child: Component) {
     const ItemContainer = this.element.querySelector('.page-item_body')! as HTMLElement;
     child.attachTo(ItemContainer);
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
